ca9506dc95a8b7a7302f152a2f8ac3dcb76eaf70
royalknight71 <krishgupta22234990@gmail.com>
2026-06-19 12:54:17 +0530

    Rendered player cards dynamically using map

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,23 @@
 import './App.css'
 import PlayerCard from "./components/PlayerCard.jsx"
 function App() {
-  
+  const players=[
+    {
+    name: "Rohit Sharma",
+    runs: 11168,
+    role: "Batsman"
+    },
+    {
+      name: "Virat Kohli",
+      runs: 14085,
+      role: "Batsman"
+    },
+    {
+      name: "Jasprit Bumrah",
+      runs: 234,
+      role: "Bowler"
+    }
+  ]
 
   return (
     <>
@@ -9,19 +25,17 @@ function App() {
       <br />
       <h2>Top Players</h2>
       <br />
-      {/* <h2>Rohit Sharma</h2>
-      <h2>Virat Kohli</h2>
-      <h2>Jasprit Bumrah</h2>
-      <h2>Hardik Pandya</h2> */}
-      <PlayerCard
-        name="Rohit Sharma"
-        runs="11168"
-        role="Batsman"
+      
+        {players.map((player)=>{
+          return (<PlayerCard
+          key={player.name}
+        name={player.name}
+        runs={player.runs}
+        role={player.role}
       />
-      <PlayerCard name="Virat Kohli" runs="14085" role="Batsman"
-      />
-      <PlayerCard name="Jasprit Bumrah" runs="234" role="Bowler"
-      />
+    )
+      })}
+      
       
     </>
   )
