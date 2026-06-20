9de00e1549774d45158515a82f0f11269304f23a
royalknight71 <krishgupta22234990@gmail.com>
2026-06-20 13:50:55 +0530

    Added search and role filtering functionality

--- src/App.jsx
+++ src/App.jsx
@@ -70,11 +70,16 @@ function App() {
   ]
 
   const [searchTerm,setsearchTerm]=useState("")
+  const [selectedRole,setSelectedRole]=useState("All")
   const filterplayer=players.filter((player)=>{
-    return player.name.toLowerCase().includes(searchTerm.toLowerCase())
+    const matchSearch= player.name.toLowerCase().includes(searchTerm.toLowerCase())
+
+    const matchRole=selectedRole==="All"||selectedRole===player.role
+
+    return matchSearch&&matchRole
   })
   
-
+  
   return (
     <>
       <h1>Criclyst</h1>
@@ -91,6 +96,19 @@ function App() {
             setsearchTerm(e.target.value)
           }}
         />
+      </div>
+      <div className="filter-container">
+        <select name="all-players" id="all-players" className="role-filter"
+        value={selectedRole}
+        onChange={(e)=>{
+          setSelectedRole(e.target.value)
+        }}>
+          <option value="All">All Players</option>
+          <option value="Batsman">Batsman</option>
+          <option value="Bowler">Bowler</option>
+          <option value="All-Rounder">All-Rounder</option>
+          <option value="Wicket Keeper">Wicket Keeper</option>
+        </select>
       </div>
         {filterplayer.map((player)=>{
           return (<PlayerCard
