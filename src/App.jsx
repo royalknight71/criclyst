c2a16a73dc3325699ee0ccb41bf2d94c1e4815d7
royalknight71 <krishgupta22234990@gmail.com>
2026-06-23 11:43:55 +0530

    Added player comparison with stat highlights

--- src/App.jsx
+++ src/App.jsx
@@ -8,6 +8,9 @@ function App() {
   const [searchTerm,setsearchTerm]=useState("")
   const [selectedRole,setSelectedRole]=useState("All")
   const [selectedPlayer, setSelectedPlayer]=useState(null)
+  const [player1,setPlayer1]=useState("")
+  const [player2,setPlayer2]=useState("")
+
   const filterplayer=players.filter((player)=>{
     const matchSearch= player.name.toLowerCase().includes(searchTerm.toLowerCase())
     const matchRole=selectedRole==="All"||selectedRole===player.role
@@ -15,7 +18,36 @@ function App() {
     return matchSearch&&matchRole
   })
   
-  
+    const player1Data = players.find(
+      (player) => player.name === player1
+    )
+
+    const player2Data = players.find(
+      (player) => player.name === player2
+    )
+      let player1WinsRuns = false
+      let player2WinsRuns = false
+      let player1WinAvg=false
+      let player2WinAvg=false
+      let player1SR=false
+      let player2SR=false
+      let player1MatchWin = false
+      let player2MatchWin = false
+      let player1WktWin=false
+      let player2WktWin=false
+      if(player1Data && player2Data)
+      {
+        player1WinsRuns = player1Data.runs > player2Data.runs
+        player2WinsRuns = player2Data.runs > player1Data.runs
+        player1WktWin = player1Data.wickets > player2Data.wickets
+        player2WktWin = player2Data.wickets > player1Data.wickets
+        player1WinAvg=player1Data.average>player2Data.average
+        player2WinAvg=player1Data.average<player2Data.average
+        player1SR=player1Data.strikeRate>player2Data.strikeRate
+        player2SR=player1Data.strikeRate<player2Data.strikeRate
+          player1MatchWin = player1Data.matches > player2Data.matches
+          player2MatchWin = player1Data.matches < player2Data.matches
+      }
   return (
     <>
       <h1>Criclyst</h1>
@@ -56,22 +88,109 @@ function App() {
       <br />
       <div className="compare-container">
         <h2>Player Comparison</h2>
-        <select name="player1comp" id="player1comp">
+        <select name="player1comp" id="player1comp"
+        value={player1}
+        onChange={(e)=>{
+          setPlayer1(e.target.value)
+        }}
+        >
+          <option value="">
+            Select Player 1
+          </option>
           {players.map((player) => (
               <option key={player.name} value={player.name}>
                 {player.name}
               </option>
             ))}
         </select>
-        <select name="player2comp" id="player2comp">
+        <select name="player2comp" id="player2comp"
+        value={player2}
+        onChange={(e)=>{
+          setPlayer2(e.target.value)
+        }}
+        > 
+          <option value="">
+            Select Player 2
+          </option>
           {
             players.map((player)=>(
-              <option key={player.name} value="{player.name}">
+              <option key={player.name} value={player.name}>
                 {player.name}
               </option>
             ))
           }
         </select>
+      </div>
+      <div>
+        {
+        (player1Data && player2Data)
+        ? 
+        <div className="comparison-card">
+          <div className="compare-title">
+            <span>{player1Data.name}</span>
+            <span className="vs-text">VS</span>
+            <span>{player2Data.name}</span>
+          </div>
+        <div className="comparison-row">
+          <span>Runs</span>
+
+          <span>
+            {player1Data.runs}
+            {player1WinsRuns && <span className="winner-dot"></span>}
+          </span>
+
+          <span>
+            {player2Data.runs}
+            {player2WinsRuns && <span className="winner-dot"></span>}
+          </span>
+        </div>
+        <div className="comparison-row">
+          <span>Wickets</span>
+
+          <span>
+            {player1Data.wickets}
+            {player1WktWin && <span className="winner-dot"></span>}
+          </span>
+
+          <span>
+            {player2Data.wickets}
+            {player2WktWin && <span className="winner-dot"></span>}
+          </span>
+        </div>
+
+          <div className="comparison-row">
+            <span>Matches</span>
+            <span>{player1Data.matches}
+              {player1MatchWin && <span className="winner-dot"></span>}
+            </span>
+            <span>{player2Data.matches}
+              {player2MatchWin && <span className="winner-dot"></span>}
+            </span>
+          </div>
+
+          <div className="comparison-row">
+            <span>Average</span>
+            
+            <span>{player1Data.average}
+              {player1WinAvg&&<span className="winner-dot"></span>}
+            </span>
+            <span>{player2Data.average}
+              {player2WinAvg&&<span className="winner-dot"></span>}
+            </span>
+          </div>
+
+          <div className="comparison-row">
+            <span>Strike Rate</span>
+            <span>{player1Data.strikeRate}
+              {player1SR&&<span className="winner-dot"></span>}
+            </span>
+            <span>{player2Data.strikeRate}
+              {player2SR&&<span className="winner-dot"></span>}
+            </span>
+          </div>
+        </div>
+        : <p></p>
+      }
       </div>
       {
       (filterplayer.length===0)
