2bac204b9f503771969ec55c46bd49716379c29c
royalknight71 <krishgupta22234990@gmail.com>
2026-07-13 11:08:17 +0530

    feat: implement Redis sliding window rate limiter

--- backend/middleware/rateLimiter.js
+++ backend/middleware/rateLimiter.js
@@ -1,27 +1,48 @@
 import redisClient from "../config/redis.js"
 
+const windowSize=3600
+const maxRequest=60
 
 const rateLimiter=async (req,res,next)=>{
     try{
-        const ip=req.ip
-        console.log(ip);
+        // const ip=req.ip
+        // console.log(ip);
         
-        const cnt=await redisClient.incr(ip)
+        // const cnt=await redisClient.incr(ip)
 
-        if(cnt>60)
+        // if(cnt>60)
+        // {
+        //     return res.status(429).json({
+        //         success: false,
+        //         message: "Too many requests. Please try again later."
+        //     });
+        // }
+            
+
+        // if(cnt==1)
+        //     await redisClient.expire(ip, 3600);
+
+        // console.log(cnt);
+        
+        const key=`$req.ip`
+        const currentTime=Date.now()/1000
+        const windowTime=currentTime-windowSize
+
+        await redisClient.zRemRangeByScore(key,0,windowTime)
+
+        const numberOfRequest=await redisClient.zCard(key)
+        if(numberOfRequest>=maxRequest)
         {
             return res.status(429).json({
                 success: false,
                 message: "Too many requests. Please try again later."
             });
         }
-            
 
-        if(cnt==1)
-            await redisClient.expire(ip, 3600);
+        await redisClient.zAdd(key,[{score:currentTime,value:`${currentTime}:${Math.random()}`}])
 
-        console.log(cnt);
-        
+        await redisClient.expire(key,windowSize)
+
         next()
     }
     catch(err){
